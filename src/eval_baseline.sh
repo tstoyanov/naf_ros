af78675b90083664796d21ce7d7da39f2f7cf501
Todor Stoyanov <todor.stoyanov@oru.se>
2020-07-09 18:01:02 +0200

    Initializing PyCharm project

--- src/eval_baseline.sh
+++ src/eval_baseline.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 source /home/aass/pandarl_ws/devel/setup.bash
 echo $ROS_PACKAGE_PATH
-for seed in 1234 2341 3412; do
-  for a_scale in 1 10 100 ; do
-    for u_step in 1 5 10 ; do
-       /home/aass/anaconda3/envs/naf_ros/bin/python main.py --env 2DProblem --seed ${seed} --num_episodes 100 --action_scale ${a_scale} --logdir \
-/home/aass/naf_logs --updates_per_step ${u_step}
+#for seed in 1234 2341 3412; do
+for kd in 0 1 10 100; do
+  for a_scale in 10; do # 100 ; do
+    for u_step in 5; do #1 5 10 ; do
+       /home/aass/anaconda3/envs/naf_ros/bin/python main.py --env 2DProblem --seed 1234 --kd ${kd} --num_episodes 100 --action_scale ${a_scale} --logdir \
+/home/aass/naf_logs/${kd} --updates_per_step ${u_step}
     done
   done
 done
