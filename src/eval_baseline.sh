f264b87ff5bfaebc24b529b1e1f84e8632ca2537
Todor Stoyanov <todor.stoyanov@oru.se>
2020-07-13 18:34:27 +0200

    typo

--- src/eval_baseline.sh
+++ src/eval_baseline.sh
@@ -6,7 +6,7 @@ for kd in 0 10; do
   for a_scale in 100; do # 100 ; do
     for u_step in 10 50; do #1 5 10 ; do
        /home/aass/anaconda3/envs/naf_ros/bin/python main.py --env 2DProblem --seed 2341 --kd ${kd} --num_episodes 300 --action_scale ${a_scale} --logdir \
-/home/aass/naf_logs/ --updates_per_step ${u_step} --exploration-end 200
+/home/aass/naf_logs/ --updates_per_step ${u_step} --exploration_end 200
     done
   done
 done
