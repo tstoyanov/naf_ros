6367062f87706855cacbaa805b2d331898e6b717
Todor Stoyanov <todor.stoyanov@oru.se>
2020-07-24 18:50:53 +0200

    halfspace intersection code starting integration. note this version does not work at the moment!

---
+++ src/quad.py
@@ -0,0 +1,12 @@
+import numpy as np
+import quadprog as qp
+
+def project_action(action,Ax,bx):
+    ndim = np.shape(action)[0]
+    qp_G = np.identity(ndim)
+    qp_a = np.array(action,dtype="float64")
+    qp_C = np.array(-Ax.T,dtype="float64")
+    qp_b = np.array(-bx,dtype="float64")
+    meq = 0
+    solution = qp.solve_qp(qp_G,qp_a,qp_C,qp_b,meq)
+    return solution[0]
